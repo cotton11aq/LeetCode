b227341274a8b95d235331b5a72af7b1fbfcd797
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-01-15 07:01:04 +0900

    追加

--- js/index copy.jsx
+++ js/index copy.jsx
@@ -1,12 +1,13 @@
 function main(input) {
   var args = input.split("\n");
-  var n = parseInt(args[0], 10);
-  var d = args.slice(1, args.length - 1).map((n) => parseInt(n, 10)).sort((a, b) => a - b);
+  var c = args[0].split(" ").map((n) => parseInt(n, 10));
+  var a = args[1].split(" ");
 
-  var sum = 1;
-  for (let i = 1; i < n; i++) {
-    if (d[i - 1] < d[i]) sum++;
+  var sum = 0;
+  for (let i = 0; i < args[0].length; i++) {
+    if (a[i] === "Alice") sum += c[i];
   }
+
   console.log(sum);
 }
 
