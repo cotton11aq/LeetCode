b9e87fd04d5258bf6b6280401cf7fed49ec9de01
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-01-18 07:32:01 +0900

    追加

--- js/index copy.jsx
+++ js/index copy.jsx
@@ -1,14 +1,25 @@
 function main(input) {
-  var args = input.split("\n");
-  var c = args[0].split(" ").map((n) => parseInt(n, 10));
-  var a = args[1].split(" ");
+  input = input.trim().split("\n");
+  var n = parseInt(input[0], 10);
 
-  var sum = 0;
-  for (let i = 0; i < args[0].length; i++) {
-    if (a[i] === "Alice") sum += c[i];
+  var list = [[0, 0, 0]];
+  for (var i = 1; i <= n; i++) {
+    var t = Number(input[i].split(" ")[0]);
+    var x = Number(input[i].split(" ")[1]);
+    var y = Number(input[i].split(" ")[2]);
+    list.push([t, x, y]);
   }
+  for (var i = 0; i < n; i++) {
+    dt = list[i + 1][0] - list[i][0];
+    dx = Math.abs(list[i + 1][1] - list[i][1]);
+    dy = Math.abs(list[i + 1][2] - list[i][2]);
 
-  console.log(sum);
+    dis = dx + dy;
+    if (dis > dt || dt % 2 != dis % 2) {
+      console.log("No");
+      return false;
+    }
+  }
+  console.log("Yes");
 }
-
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
