dc14a36da20b4d4d6bd4d3ef650b0e18d37fb8bc
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-01-25 22:12:52 +0900

    修正

--- js/index copy.jsx
+++ js/index copy.jsx
@@ -1,32 +1,5 @@
 function main(input) {
-  var args = input.split("\n").map((n) => n.split(" "));
-  var n = args[0][0];
-  var x = args[0][1];
-  var v = args.slice(1, n + 1).map((n) => parseInt(n[0], 10));
-  var p = args.slice(1, n + 1).map((n) => parseInt(n[1], 10));
-  
-  console.log(args);
-  console.log(n);
-  console.log(x);
-  console.log(v);
-  console.log(p);
 
-  var sum = 0;
-  var count = 0;
-  var bool = false;
-  for (let i = 0; i < n; i++) {
-    if (sum <= x) {
-      sum += v[i] * (p[i] / 100);
-      count++;
-    } else {
-      break;
-    }
-  }
-  if (sum > x) {
-    console.log(count);
-  } else {
-    console.log(-1);
-  }
 }
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
