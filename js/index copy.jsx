8e7b809790f91ce8f9b7acae118218c48cf708e7
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-01-27 22:26:22 +0900

    修正

--- js/index copy.jsx
+++ js/index copy.jsx
@@ -4,8 +4,6 @@ function main(input) {
   var args = input.split("\n");
   var a = parseInt(args[0], 10);
   var b = parseInt(args[1], 10);
-  var c = parseInt(args[2], 10);
-  var x = parseInt(args[3], 10);
 
   
 }
