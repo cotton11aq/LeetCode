b01b0dd238e8504bb6b08d2024f7a2e1c15ef099
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-02-02 16:30:56 +0900

    追加

--- js/index copy.jsx
+++ js/index copy.jsx
@@ -3,13 +3,13 @@ function main(input) {
   var n = parseInt(args[0], 10);
   var a = args[1].split(" ").map((n) => parseInt(n, 10));
 
-  var sum = 0;
+  var count = 0;
   while (a.every((n) => n % 2 === 0)) {
     a = a.map((n) => (n /= 2));
-    sum++;
+    count++;
   }
 
-  console.log(sum);
+  console.log(count);
 }
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
