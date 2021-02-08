e61c5eae967e063f892eacbab6a9dc8549878f79
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-02-08 21:26:16 +0900

    追加

--- js/index copy.jsx
+++ js/index copy.jsx
@@ -1,10 +1,16 @@
+const { func } = require("prop-types");
+
 function main(input) {
   var args = input.split(" ");
   var n = parseInt(args[0], 10);
   var a = parseInt(args[1], 10);
   var b = parseInt(args[2], 10);
 
-  
+  $(function () {
+    $("#q10").click(function () {
+      $(this).toggleClass("bbb");
+    });
+  });
 }
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
