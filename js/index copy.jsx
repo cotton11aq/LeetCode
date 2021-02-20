4f2f68defb412a85fb3b16b2ff8d213a22899b1c
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-02-21 07:30:40 +0900

    追加

--- js/index copy.jsx
+++ js/index copy.jsx
@@ -1,14 +1,35 @@
-const { func } = require("prop-types");
-
 function main(input) {
-  var args = input.split(" ");
-  var n = parseInt(args[0], 10);
-  var a = parseInt(args[1], 10);
-  var b = parseInt(args[2], 10);
+  let fa = input.split("\n");
+  let args = fa[0];
 
-  $(function () {
-    $("<p>くくく</p>").insertAffter("div #q17");
-  });
+  let hoge = [];
+  let fuga = [];
+
+  for (let i = 0; i < args.length; i++) {
+    if (i % 2 === 0) {
+      hoge.push(args[i]);
+    } else {
+      fuga.push(args[i]);
+    }
+  }
+
+  hoge = hoge.join("");
+  fuga = fuga.join("");
+
+  const isUpperCase = (c) => {
+    return /^[A-Z]+$/g.test(c);
+  };
+
+  const isUpperCase02 = (c) => {
+    return /^[a-z]+$/g.test(c);
+  };
+  if (fuga === "" && isUpperCase02(hoge) == true) {
+    console.log("Yes");
+  } else if (isUpperCase02(hoge) == true && isUpperCase(fuga) == true) {
+    console.log("Yes");
+  } else {
+    console.log("No");
+  }
 }
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
