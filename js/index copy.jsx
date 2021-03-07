323c8a7ffda25bdf7b3c921f4d14b55774c4f9ec
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-03-08 06:44:38 +0900

    成功

--- js/index copy.jsx
+++ js/index copy.jsx
@@ -1,19 +1,25 @@
-function main(input) {
+function Main(input) {
   let args = input.split("\n");
   let n = parseInt(args[0], 10);
-  let nums = args.slice(1, n + 1).map((n) => n.split(" "));
-  let a = nums.map((n) => parseInt(n[0], 10));
-  let b = nums.map((n) => parseInt(n[1], 10));
+  let num = args.slice(1, n + 1).map((n) => n.split(" "));
+  let a = num.map((n) => parseInt(n[0], 10));
+  let b = num.map((n) => parseInt(n[1], 10));
 
   let min = a[0] + b[0];
 
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < a.length; j++) {
-      min = Math.min(min, a[i] + b[j]);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let summin = 0;
+      if (i === j) {
+        summin = a[i] + b[j];
+      } else {
+        summin = Math.max(a[i], b[j]);
+      }
+      min = Math.min(min, summin);
     }
   }
 
   console.log(min);
 }
 
-main(require("fs").readFileSync("/dev/stdin", "utf8"));
+Main(require("fs").readFileSync("/dev/stdin", "utf8").trim());
